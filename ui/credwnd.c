74f7f6719729d9e945f6eeae12d1fadc7de271b3
Asanka Herath <asanka@secure-endpoints.com>
2007-08-14 19:19:16 +0000

    Updates for NIM credentials view:
    1. Credentials and expanded headers that show countdown clocks (time left) need to be redrawn periodically.  This is done by setting a timer for a row that fires when the clock on that that row needs to be redrawn.  When switching between the basic and advanced view, the timers need to the discarded because the rows and clocks that are displayed will change.  However, if a timer fires just as we are switching modes, the associated WM_TIMER message will be placed on the message queue and will not be removed when we try to kill the timer using KillTimer().  Therefore, we might receive a WM_TIMER message that corresponds to a row that no longer exists, or has changed.
    
    Earlier, the code tried to assert that WM_TIMER messages we receive were consistent with current data.  Since receiving an inconsistent WM_TIMER message is not an error, this patch adds code for verifying that the timer event we have received is consistent with the data, and if not, quietly ignore the message instead of failing an assert.
    
    2. Fix a typo.  The third parameter to cw_select_row() is actually a WPARAM value containing a bit field corresponding to the modifier keys that were in effect while the selection was made.
    
    3. The user can invoke the context menu using the keyboard.  If they do, then the x and y co-ordinates provided with the WM_CONTEXTMENU message will be set to (-1, -1).  Add code to check for this situation and use the current cursor_row value as the target of the context menu.
    
    4. If the user right-clicks on a row that is not part of the current selection, the earlier code would set the cursor context to the row that the user right-clicked on while leaving the selection as is.  Since some actions (such as 'new credentials' and 'properties') use the cursor context and some other actions (such as 'destroy credentials') use the selection context, this results in inconsistent behavior.
    
    This patch adds code that update the selection to include the row that the user right-clicked on if that row is not part of the selection.  Doing so would provide a consistent experience where actions that operate on the cursor context and actions that operate on the selection both operate on the same credentials that the user sees as selected.
    
    5.  Remove unused code.

--- ui/credwnd.c
+++ ui/credwnd.c
@@ -1194,11 +1194,21 @@ cw_timer_proc(HWND hwnd,
         FILETIME ft_now;
 
         o = (khui_credwnd_outline *) r->data;
-#ifdef DEBUG
-        assert(r->flags & KHUI_CW_ROW_EXPVIEW);
-        assert(o->attr_id == KCDB_ATTR_ID);
-        assert(tbl->flags & KHUI_CW_TBL_EXPIDENT);
-#endif
+
+        /* we only handle timers for header rows if :
+
+           1.  The table is displaying expanded identity information
+           2.  The row displaying an expanded view
+           3.  The relevant outline object is for an identity
+
+           If these conditions aren't met, it is because we lost a
+           race killing this timer while switching modes.
+         */
+        if (!(tbl->flags & KHUI_CW_TBL_EXPIDENT) ||
+            !(r->flags & KHUI_CW_ROW_EXPVIEW) ||
+            !(o->attr_id == KCDB_ATTR_ID))
+
+            return;
 
         nflags = cw_get_buf_exp_flags(tbl, (khm_handle) o->data);
         if ((o->flags & CW_EXPSTATE_MASK) != nflags) {
@@ -5115,7 +5125,7 @@ cw_wm_command(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             cw_update_creds(tbl);
             cw_update_outline(tbl);
             cw_update_extents(tbl, TRUE);
-            cw_select_row(tbl, tbl->cursor_row, TRUE);
+            cw_select_row(tbl, tbl->cursor_row, 0);
             cw_update_selection_state(tbl);
 
             InvalidateRect(tbl->hwnd, NULL, TRUE);
@@ -5133,7 +5143,7 @@ cw_wm_command(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             cw_update_creds(tbl);
             cw_update_outline(tbl);
             cw_update_extents(tbl, TRUE);
-            cw_select_row(tbl, tbl->cursor_row, TRUE);
+            cw_select_row(tbl, tbl->cursor_row, 0);
             cw_update_selection_state(tbl);
 
             InvalidateRect(tbl->hwnd, NULL, TRUE);
@@ -5152,7 +5162,7 @@ cw_wm_command(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             cw_update_creds(tbl);
             cw_update_outline(tbl);
             cw_update_extents(tbl, TRUE);
-            cw_select_row(tbl, tbl->cursor_row, TRUE);
+            cw_select_row(tbl, tbl->cursor_row, 0);
             cw_update_selection_state(tbl);
 
             InvalidateRect(tbl->hwnd, NULL, TRUE);
@@ -5171,7 +5181,7 @@ cw_wm_command(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             cw_update_creds(tbl);
             cw_update_outline(tbl);
             cw_update_extents(tbl, TRUE);
-            cw_select_row(tbl, tbl->cursor_row, TRUE);
+            cw_select_row(tbl, tbl->cursor_row, 0);
             cw_update_selection_state(tbl);
 
             InvalidateRect(tbl->hwnd, NULL, TRUE);
@@ -5190,7 +5200,7 @@ cw_wm_command(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             cw_update_creds(tbl);
             cw_update_outline(tbl);
             cw_update_extents(tbl, TRUE);
-            cw_select_row(tbl, tbl->cursor_row, TRUE);
+            cw_select_row(tbl, tbl->cursor_row, 0);
             cw_update_selection_state(tbl);
 
             InvalidateRect(tbl->hwnd, NULL, TRUE);
@@ -5209,7 +5219,7 @@ cw_wm_command(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             cw_update_creds(tbl);
             cw_update_outline(tbl);
             cw_update_extents(tbl, TRUE);
-            cw_select_row(tbl, tbl->cursor_row, TRUE);
+            cw_select_row(tbl, tbl->cursor_row, 0);
             cw_update_selection_state(tbl);
 
             InvalidateRect(tbl->hwnd, NULL, TRUE);
@@ -5434,6 +5444,33 @@ cw_wm_contextmenu(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     x = GET_X_LPARAM(lParam);
     y = GET_Y_LPARAM(lParam);
 
+    /* if the user invokes the context menu using the keyboard, we get
+       x=-1 and y=-1.  In this case, we use the cursor_row as the
+       target row. */
+    if (x == -1 && y == -1) {
+
+        row = tbl->cursor_row;
+
+        if (tbl->flags & KHUI_CW_TBL_EXPIDENT) {
+            int i;
+
+            y = 0;
+            for (i=0; i < tbl->n_rows && i < row; i++) {
+                if (tbl->rows[i].flags & KHUI_CW_ROW_EXPVIEW)
+                    y += tbl->cell_height * CW_EXP_ROW_MULT;
+                else
+                    y += tbl->cell_height;
+            }
+        } else {
+            y = row * tbl->cell_height;
+        }
+
+        x = r.left;
+        y = y + r.top + tbl->header_height - tbl->scr_top;
+        
+        goto have_row;
+    }
+
     x += tbl->scr_left - r.left;
     y += tbl->scr_top - tbl->header_height - r.top;
 
@@ -5468,23 +5505,21 @@ cw_wm_contextmenu(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     if(row < 0 || row >= (int) tbl->n_rows)
         return FALSE;
 
+    /* now, if the user has right clicked outside the selection, we
+       treat the right-click as a regular click before showing the
+       context menu. */
+    if (!(tbl->rows[row].flags & KHUI_CW_ROW_SELECTED)) {
+        cw_select_row(tbl, row, 0);
+    }
+
+    x = GET_X_LPARAM(lParam);
+    y = GET_Y_LPARAM(lParam);
+
+ have_row:
+
     cw_set_row_context(tbl, row);
 
-    khm_menu_show_panel(KHUI_MENU_IDENT_CTX, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
-
-#if 0
-    /* calling cw_set_row_context() should take care of enabling or
-       disabling actions as appropriate.  We don't need to
-       differentiate between IDENT_CTX and TOK_CTX here. */
-    if((tbl->rows[row].flags & KHUI_CW_ROW_HEADER) &&
-       (tbl->cols[tbl->rows[row].col].attr_id == KCDB_ATTR_ID_NAME)) {
-        khm_menu_show_panel(KHUI_MENU_IDENT_CTX, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
-        //khui_context_reset();
-    } else {
-        khm_menu_show_panel(KHUI_MENU_TOK_CTX, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
-        //khui_context_reset();
-    }
-#endif
+    khm_menu_show_panel(KHUI_MENU_IDENT_CTX, x, y);
 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
