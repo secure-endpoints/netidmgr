3f59842290da4d1aa950f632f52c4f9ff382c545
Asanka Herath <asanka@secure-endpoints.com>
2007-08-27 23:44:23 +0000

    fix path

--- startdev.cmd
+++ startdev.cmd
@@ -1,4 +1,5 @@
 start /MIN cmd /K "cd obj\i386\dbg && Title NetIDMgr OBJ"
+set PATH=%PATH%;c:\work\svn\bin
 start /MIN cmd /K "cd ..\..\..\..\krb5-svn\krb5-trunk\src\windows\identity && Title NetIDMgr SVN"
 call setupbuild.cmd
 set PATH=%PATH%;c:\work\global\bin
