74a7e5f9456c0a639bf39dd1a92e92bc8ec782e5
Asanka Herath <asanka@secure-endpoints.com>
2009-01-15 06:01:37 +0000

    Don't open the SVN directory.  It's no longer necessary.

--- startdev.cmd
+++ startdev.cmd
@@ -1,4 +1,3 @@
 call setupbuild.cmd
 start /MIN cmd /K "cd obj\i386\dbg && Title NetIDMgr OBJ"
-start /MIN cmd /K "cd ..\..\..\..\krb5-svn\krb5-trunk\src\windows\identity && Title NetIDMgr SVN"
 runemacs
