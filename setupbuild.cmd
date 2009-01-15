1d89ebb024d76427ae7561dcecd2633f7b8bbf62
Asanka Herath <asanka@secure-endpoints.com>
2009-01-15 06:00:59 +0000

    Remove AFS build configuration parameters and a couple of other legacy cruft.

--- setupbuild.cmd
+++ setupbuild.cmd
@@ -3,14 +3,13 @@ set PATH=%PATH%;c:\work\svn\bin
 set PISMERE=c:\work\pismere
 set KH_DOXYFULLPATH=c:\work\doxygen\doxygen.exe
 set KH_HHCFULLPATH="c:\Program Files\HTML Help Workshop\hhc.exe"
+set KH_ROOT=%CD%
 
 if "%1"=="retail" (
 call "%PROGRAMFILES%\Microsoft Platform SDK for Windows Server 2003 R2\setenv.cmd" /XP32 /RETAIL
-set KH_AFSPATH=c:\work\openafs.cvshead\openafs\dest\i386_nt40\free
 title NetIDMgr Build [RETAIL XP32]
 ) else (
 call "%PROGRAMFILES%\Microsoft Platform SDK for Windows Server 2003 R2\setenv.cmd" /XP32 /DEBUG
-set KH_AFSPATH=c:\work\openafs.cvshead\openafs\dest\i386_nt40\checked
 title NetIDMgr Build [DEBUG XP32]
 )
 
