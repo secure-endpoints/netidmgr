acdd097979979e8bc3a83f0da43a097124db563e
Asanka Herath <asanka@secure-endpoints.com>
2009-02-04 00:08:28 +0000

    Specify optimization options for PGO

--- setupbuild.cmd
+++ setupbuild.cmd
@@ -5,6 +5,7 @@ set KH_DOXYFULLPATH=c:\work\doxygen\doxygen.exe
 set KH_HHCFULLPATH="c:\Program Files\HTML Help Workshop\hhc.exe"
 set KH_ROOT=%CD%
 set KH_NO_W2K=1
+set KH_CLVER=vc8
 
 if "%1"=="retail" (
 call "%PROGRAMFILES%\Microsoft Platform SDK for Windows Server 2003 R2\setenv.cmd" /XP32 /RETAIL
