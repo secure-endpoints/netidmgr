9e99fbd4af3aa6f40c10c496fd6f1b41a557428c
Asanka Herath <asanka@secure-endpoints.com>
2009-10-07 10:53:57 +0000

    Visibility is a property that's set based on the condition in a FlowLayout.

--- ui/FlowLayout.hpp
+++ ui/FlowLayout.hpp
@@ -145,7 +145,7 @@ namespace nim {
         FlowLayout& Add(DisplayElement * e, Alignment opt = Left,
                         Type etype = Fixed, bool condition = true) {
 
-            if (!condition || !e || !e->visible) {
+            if (!condition || !e) {
                 if (e)
                     e->visible = false;
                 return *this;
