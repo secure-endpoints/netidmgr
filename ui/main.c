6771d2fceab05753a47f488210a13dd202ea7f41
Asanka Herath <asanka@secure-endpoints.com>
2009-10-09 05:23:11 +0000

    .

--- ui/main.c
+++ ui/main.c
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 2005 Massachusetts Institute of Technology
- *
- * Copyright (2) 2007 Secure Endpoints Inc.
+ * Copyright (c) 2007 Secure Endpoints Inc.
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
