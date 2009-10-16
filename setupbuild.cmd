cb8d1605c5bd9e41141a36e325234050412a234e
Asanka Herath <asanka@secure-endpoints.com>
2009-10-16 21:21:29 +0000

    Fix path for Keystore binaries used when building ZIP file.
    Set ZIP variable when setting up the environment to point to the location of the zip tool.

--- setupbuild.cmd
+++ setupbuild.cmd
@@ -7,6 +7,7 @@ set KH_ROOT=%CD%
 set KH_NO_W2K=1
 set KH_CLVER=vc9
 set WIXDIR=c:\work\wix2
+set ZIP=c:\Program Files\7-Zip\7z.exe
 
 if "%1"=="retail" (
 call D:\WindowsSDK\v6.1\Bin\SetEnv.Cmd /x86 /xp /Release
