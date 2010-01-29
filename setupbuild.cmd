6f22249dbee89b2149a91fb2527eb7fe5dcc190b
Asanka Herath <asanka@secure-endpoints.com>
2010-01-29 01:50:41 -0500

    New build setup script
    The old build setup script was specific to my machine and had to
    be modified each time I changed machines or even build
    directories.  With this script, hopefully we will be able to use
    a single master build setup script.
    
    Any machine specific setup is now done in setupbuild.local.cmd
    which is called from setupbuild.cmd.  The arguments to
    setupbuild.cmd are the same as the arguments to the Platform SDK
    build environment.  All the build type values are derived from
    the variables setup by the Platform SDK SetEnv.Cmd.
    
    We support Platform SDK version v6.1 for now.

--- setupbuild.cmd
+++ setupbuild.cmd
@@ -1,31 +1,74 @@
-rem Setup build environment for NetIDMgr
+@echo off
 
-set PATH=%PATH%;c:\work\svn\bin
+set PSDKDir=%PROGRAMFILES%\Microsoft SDKs\Windows\v6.1\a
 
-set PISMERE=c:\work\pismere
-set KH_DOXYFULLPATH=c:\work\doxygen\doxygen.exe
-set KH_HHCFULLPATH="c:\Program Files\HTML Help Workshop\hhc.exe"
-set KH_ROOT=%CD%
-set KH_NO_W2K=1
-set KH_CLVER=vc9
-set WIXDIR=c:\work\wix2
-set ZIP=c:\Program Files\7-Zip\7z.exe
-set CODESIGN=c:\work\codesign\codesign_nim.cmd
+for /F "tokens=2* delims=	 " %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Microsoft SDKs\Windows\v6.1\WinSDKWin32Tools" /v InstallationFolder') do set PSDKDir=%%B
 
-if "%1"=="retail" (
-call D:\WindowsSDK\v6.1\Bin\SetEnv.Cmd /x86 /xp /Release
-title NetIDMgr Build [Release XP 32]
+if exist "%PSDKDir%bin\SetEnvVS8.Cmd" (
+  call "%PSDKDir%bin\SetEnvVS8.Cmd" %1 %2 %3 %4
 ) else (
-call D:\WindowsSDK\v6.1\Bin\SetEnv.Cmd /x86 /xp /Debug
-title NetIDMgr Build [Debug XP 32]
+  call "%PSDKDir%bin\SetEnv.Cmd" %1 %2 %3 %4
 )
 
-rem if "%1"=="retail" (
-rem call "%PROGRAMFILES%\Microsoft Platform SDK for Windows Server 2003 R2\setenv.cmd" /XP32 /RETAIL
-rem title NetIDMgr Build [RETAIL XP32]
-rem ) else (
-rem call "%PROGRAMFILES%\Microsoft Platform SDK for Windows Server 2003 R2\setenv.cmd" /XP32 /DEBUG
-rem title NetIDMgr Build [DEBUG XP32]
-rem )
+cl.exe 2> "%TEMP%\clver.txt" 1> NUL
+
+for /F "tokens=7,8* delims=. " %%A in (%TEMP%\clver.txt) do (
+  if "%%A"=="Version" (
+    if "%KH_CLVER%"=="" (
+      if "%%B"=="14" set KH_CLVER=vc8
+      if "%%B"=="15" set KH_CLVER=vc9
+      if "%%B"=="16" set KH_CLVER=vc10
+    )
+  ) else (
+    if "%KH_CLVER%"=="" (
+      if "%%A"=="14" set KH_CLVER=vc8
+      if "%%A"=="15" set KH_CLVER=vc9
+      if "%%A"=="16" set KH_CLVER=vc10
+    )
+  )
+)
+
+if "%KH_CLVER%"=="" echo Can't determine Visual C compiler version.  Please set KH_CLVER manually to vc8, vc9 or vc10.
+
+del /q "%TEMP%\clver.txt"
+
+set KH_ROOT=%CD%
 
 set NODOCBUILD=1
+if "%NODEBUG%"=="1" (
+set KH_BUILD=RETAIL
+) else (
+set KH_BUILD=DEBUG
+)
+set KH_NO_W2K=1
+
+set KH_RELEASE=PRERELEASE
+
+set KH_AUXCFLAGS=/wd4512 /wd4267 /wd4389 /wd4245 /wd4311 /wd4127 /wd4115 /wd4100
+
+rem ------------------------------------------------------
+rem setupbuild.local.cmd
+rem ------------------------------------------------------
+rem
+rem  Use a setupbuild.local.cmd file to setup variables
+rem  that are specific for your build environment.
+rem  The following are required:
+rem
+rem  - perl.exe should on the system path
+rem  - KH_DOXYFULLPATH should be the full path to doxygen.exe
+rem  - KH_HHCFULLPATH should be the full path to hhc.exe
+rem  - WIXDIR should be the directory containing WiX 2.x
+rem  - ZIP should be the full path to 7-Zip command line executable
+rem  - PISMERE Should be the pismere directory
+
+if exist setupbuild.local.cmd call setupbuild.local.cmd
+
+@echo off
+echo. Warning! If the mkdir being used is not what ships with Windows
+echo  clean builds will fail due to Cygwin mkdir not supporting
+echo  recursive directory creation.
+echo.
+echo  Also, the compiler may generate lots of warnings.  To suppress some
+echo  of them, the KH_AUXCFLAGS environment variable was specified with
+echo  a number of /wdxxxx flags.
+
